b32f53535b499366a98984bc13b77f8a79d59b59
Abdulrahman Alswaidan <aa2725@cornell.edu>
2025-11-20 22:57:43 -0500

    updates

--- src/Files.jl
+++ src/Files.jl
@@ -2,6 +2,10 @@ function _jld2(path::String)::Dict{String,Any}
     return load(path);
 end
 
-MyPortfolioDataSet() = _jld2(joinpath(_PATH_TO_DATA, "train_dataset_2014_2023.jld2")); # Training dataset
-MyOutOfSamplePortfolioDataSet() = _jld2(joinpath(_PATH_TO_DATA, "test_dataset_2024_onward.jld2")); # Out-of-sample dataset
-MyOriginalPortfolioDataSet() = _jld2(joinpath(_PATH_TO_DATA, "SP500-Daily-OHLC-1-3-2014-to-02-07-2025.jld2")); # Original dataset
+MyPortfolioDataSet() = _jld2(joinpath(_PATH_TO_DATA, "SP500-Daily-OHLC-1-3-2014-to-12-31-2024.jld2")); # Training dataset
+MyOutOfSamplePortfolioDataSet() = _jld2(joinpath(_PATH_TO_DATA, "SP500-Daily-OHLC-1-3-2025-to-11-18-2025.jld2")); # Out-of-sample dataset
+MyOriginalPortfolioDataSet() = _jld2(joinpath(_PATH_TO_DATA, "SP500-Daily-OHLC-1-3-2014-to-12-31-2024.jld2")); # Original dataset
+
+# MyPortfolioDataSet() = _jld2(joinpath(_PATH_TO_DATA, "train_dataset_2014_2023.jld2")); # Training dataset
+# MyOutOfSamplePortfolioDataSet() = _jld2(joinpath(_PATH_TO_DATA, "test_dataset_2024_onward.jld2")); # Out-of-sample dataset
+# MyOriginalPortfolioDataSet() = _jld2(joinpath(_PATH_TO_DATA, "SP500-Daily-OHLC-1-3-2014-to-02-07-2025.jld2")); # Original dataset
